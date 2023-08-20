3decfe2c7e7c6f8367e6b085771238284043c594
Vigneshwar <104296716+Vigneshwar2002@users.noreply.github.com>
2023-08-20 19:16:44 +0530

    Update checker.py

--- A1/checker.py
+++ A1/checker.py
@@ -1,38 +1,5 @@
-def checker(decodedName, decoderName, TransactionName):
-    decoder = {}
-    decoder_length = 0
+def checker(decodedName, outputName, TransactionName):
 
-    decoderfile = open(decoderName, 'r')
-    while True:
-        key = decoderfile.readline()
-        if not key: break
-        value = decoderfile.readline()
-        decoder[key.strip()] = [int(i) for i in value.strip().split(' ')]
-        decoder_length += (1+len(decoder[key.strip()]))
-    decoderfile.close()
-
-    decoded = []
-    decoded_length = 0
-    decodedfile = open(decodedName, 'r')
-    while True:
-        line = decodedfile.readline()
-        if not line: break
-        decoded.append(line.strip().split(' '))
-        decoded_length += len(decoded[-1])
-    decodedfile.close()
-
-    new_decoded = []
-    for i in decoded:
-        curr = []
-        for j in i:
-            try:
-                curr = curr + decoder[j]
-            except:
-                curr.append(int(j))
-        curr.sort()
-        new_decoded.append(curr)
-    
-    # new_decoded.sort()
     
     original = []
     original_length = 0
@@ -46,27 +13,47 @@ def checker(decodedName, decoderName, TransactionName):
         original_length += len(original[-1])
     originalfile.close()
 
+    regenerated = []
+    regenerated_length = 0
+    regeneratedfile = open(decodedName, 'r')
+    while True:
+        line = regeneratedfile.readline()
+        if not line: break
+        curr = [int(i) for i in line.strip().split(' ')]
+        curr.sort()
+        regenerated.append(curr)
+        regenerated_length += len(regenerated[-1])
+    regeneratedfile.close()
+
+    output_length = 0
+    outputfile = open(outputName, 'r')
+    while True:
+
+        line = outputfile.readline()
+        if not line: break
+        curr = [i for i in line.strip().split(' ')]
+        output_length += len(curr)
+    outputfile.close()
+
     # print(new_decoded)
     # print(' ')
     # print(original)
 
-    if new_decoded == original:
+    if regenerated == original:
         print("Equal")
         print("original Length ", original_length )
-        print("decoder Length ", decoder_length )
-        print("decoded Length ", decoded_length )
-        print("Total Length ", decoder_length+decoded_length )  
-        print("compression Percentage ",((original_length-decoded_length-decoder_length)*100)/original_length)
+        print("Total Length ", output_length )  
+        print("compression Percentage ",((original_length-output_length)*100)/original_length)
     else:
         print("Not Equal")
 
 
 
-decodedName = '/home/rutvik/Downloads/FP-growth-master/decoded_1500.txt'
-decoderName = '/home/rutvik/Downloads/FP-growth-master/decoder_1500.txt'
-TransactionName = "/home/rutvik/Desktop/sem7/COL761/A1/D_medium.dat"
+outputName = 'output.dat'
+decodedName = 'original.dat'
+TransactionName = "D_small.dat"
 
-checker(decodedName, decoderName, TransactionName)
+checker(decodedName, outputName, TransactionName)
 
 # A =[[1, 2, 3, 4, 5], [1, 2, 3, 4, 6], [1, 2, 3, 4, 5, 7], [1, 2, 3, 4, 5, 6, 7]]
 # A.sort()
